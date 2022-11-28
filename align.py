2c7d3c2892efd8f14a5bc00d59e91ccff53c5f69
Alcan Ünsal <alcan.unsal@buik.net>
2022-11-28 14:41:54 +0300

    Update align.py

--- align.py
+++ align.py
@@ -7,15 +7,17 @@ import numpy as np
 
 from model import Face_detect_crop
 
-face_detection_threshold = float(os.environ.get("FACE_DET_THRESHOLD", 0.4))
-similarity_threshold = float(os.environ.get("SIMILARITY_THRESHOLD", 0.2))
-face_det_size = int(os.environ.get("FACE_DET_SIZE", 320))
-face_detection_size = (face_det_size, face_det_size)
-
-crop_size = 224
 
 
-def find_faces_and_transformations(image: np.ndarray, face_align_model):
+
+def find_faces_and_transformations(
+    image: np.ndarray, 
+    face_align_model, 
+    face_detection_threshold =  0.4, 
+    similarity_threshold = 0.2, 
+    face_detection_size = (320, 320),
+    crop_size = 224):
+    
     try:
         faces, transformations = face_align_model.get(
             image, crop_size)
@@ -23,22 +25,3 @@ def find_faces_and_transformations(image: np.ndarray, face_align_model):
     except Exception as e:
         print(e)
         print(traceback.format_exc())
-
-
-if __name__ == '__main__':
-    model_path = sys.argv[1]
-    face_align_model = Face_detect_crop(
-        root=model_path)
-    face_align_model.prepare(
-        ctx_id=0, det_thresh=face_detection_threshold, det_size=face_detection_size)
-
-    imgs_paths = [pth if pth.endswith(
-        '.png') or pth.endswith(
-        '.jpg') else None for pth in os.listdir("input_photos")]
-
-    for img_path in imgs_paths:
-        if img_path is None:
-            continue
-        img = cv2.imread(img)
-        faces, _ = find_faces_and_transformations(img, face_align_model)
-        cv2.imwrite(f"aligned/{img_path}", faces[0])
